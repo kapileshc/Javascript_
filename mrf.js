d747884e46210f710ac130b1248ff0ce71034ea6
kapileshc <85663945+kapileshc@users.noreply.github.com>
2022-10-15 22:52:18 +0530

    Update mrf.js

--- mrf.js
+++ mrf.js
@@ -5,3 +5,7 @@ let arr_sqr = arr.map(num => num**2)
 console.log(arr_sqr)
 
 let arr_filter = arr.filter(num => num%2==0)
+console.log(arr_filter)
+
+let arr_sum = arr.reduce((a,b)=>a+b,0)
+console.log(arr_sum)
